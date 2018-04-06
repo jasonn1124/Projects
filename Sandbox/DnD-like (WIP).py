0e2b8ba20ba5ee155982886035ea0fa489838fb9
jasonn1124 <32764981+jasonn1124@users.noreply.github.com>
2018-04-06 17:39:21 +0800

    Add files via upload

--- Sandbox/DnD-like (WIP).py
+++ Sandbox/DnD-like (WIP).py
@@ -6,12 +6,6 @@ import subprocess
 from random import randint
 import msvcrt	# For "Press any key to continue.."s
 
-def cls():
-	subprocess.call('cls', shell = True)
-	
-def wait():
-	msvcrt.getch()
-
 class entity:
 	def __init__(
 					self, 
@@ -53,13 +47,20 @@ class entity:
 	def showStats(self):
 		print("\nName: {}\nHealth: {}/{}".format(self.name, self.currentHealth, self.maxHealth))
 		
-def display(title, header, content, response):
+def cls():
+	subprocess.call('cls', shell = True)
+	
+def wait():
+	msvcrt.getch()
+
+def display(title, content, contentHeader = "", response = ""):
 	cls()
-	print(title)
-	if header != "":
-		print(header + "\n")
+	print(title + "\n")
+	if contentHeader != "":
+		print(contentHeader + "\n")
 	print(content + "\n")
-	print(response)
+	if response != "":
+		print(response)
 	
 def checkAlive():
 	if playerChar.currentHealth == 0:
@@ -72,7 +73,8 @@ def checkAlive():
 		return False
 
 def newChar():
-	# 6 ability scores, 4d6 rolls for each, removing the lowest score and summing the rest.
+	# Generates list of 6 random ability scores from 3 to 18, inclusive.
+	# Each score is generated from 4d6, and the lowest die is removed for a total of 3d6.
 	counter_statRolls = 6
 	statRolls = []
 	while counter_statRolls != 0:
@@ -89,40 +91,74 @@ def newChar():
 		statRolls.append(summedRoll)
 		counter_statRolls -= 1	
 
-	# Prompts user to assign stat rolls to whichever stat they want.
-	statNames = ["Strength","Dexterity","Constitution","Intelligence","Wisdom","Charisma"]
+	# Allows user to assign scores to their selected stats.
+	statNames = ["Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"]
 	userChoice = []
 	counter_statInput = 0
 	while counter_statInput != 6:
-		cls()
-		print(
-			"Assign scores\n\nSelecting for {0}.\nPlease select which score should be assigned to {0}.".format(statNames[counter_statInput])
-			)
-		print("List of scores remaining: {}".format(statRolls))
+		display(
+			"Assign scores",
+			"Selecting for {0}.\nPlease select which score should be assigned to {0}.\n\nList of scores remaining: {1}"
+			.format(statNames[counter_statInput], statRolls)
+		)
+		# Input validation - Integer check
 		try:
 			userInput = int(input(">> "))
 		except ValueError:
-			print("Please enter integers only!")
+			print("\nPlease enter integers only!")
 			wait()
 		else:
+			# Input validation - check value exists
 			try:
 				statRolls.remove(userInput)
 			except ValueError:
-				print("There is no such value in the list of scores. Please pick a valid score.")
+				print("\nThere is no such value in the list of scores. Please pick a valid score.")
 				wait()
 			else:
 				userChoice.append(userInput)
 				counter_statInput += 1
+				# Checks if user is satisfied with score assignments. Else reset.
+				if counter_statInput == 6:
+					# Input validation - Y/N
+					displayContent = ""
+					for i in range(0,6):
+						displayContent += "{}: {}\n".format(statNames[i], userChoice[i])
+					isLooping = True
+					while isLooping == True:						
+						display("Assign scores", displayContent, "Ability scores", "Are you okay with these ability scores? | Y/N")
+						userInput = input(">> ")
+						try:
+							userInput = userInput.lower()
+						except ValueError:
+							print("\nPlease enter a valid response.")
+							wait()
+						else:
+							if userInput == "y":
+								# Do nothing, confirms selection
+								isLooping = False
+							elif userInput == "n":
+								counter_statInput = 0
+								statRolls = userChoice
+								userChoice = []
+								isLooping = False
+							else:
+								print("\nPlease enter a valid response..")
+								wait()
+					wait()
+				else:
+					# Do nothing
+					1 == 1
 	print(userChoice)
-			# !!! Search statRolls for userInput, if present, remove from list and assign, then loop. Else, flag error and loop without assigning.
-# App start
+			# !!! Include options to re-assign scores.
+			
+# App start, !!! display incomplete
 playerChar = entity("Hiro", 0, 0, 0, 0, 0, 0, 100, 100, True)
 enemyChar = entity("Rat", 0, 0, 0, 0, 0, 0, 10, 10, True)
 newChar()
 wait()
 isRunning = True
 while isRunning == True:
-	display()
+	display("Titlehere", "Running proper")
 	if checkAlive() == False:
 		print("\n End")
 		isRunning = False
